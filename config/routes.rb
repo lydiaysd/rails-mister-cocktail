14a1ae684cec83a2133c321498737197606a6f27
Lydia Yuzva <lydiaysd@gmail.com>
2019-05-16 11:21:06 +0100

    Rename stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: [:show, :new, :create, :index] do
-    resources :doses, only: [:new, :create]
+  resources :doses, only: [:new, :create]
+  root to: 'articles#index'
+  resources :articles, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
