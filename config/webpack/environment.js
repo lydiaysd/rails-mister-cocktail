050343d836c7090f04d7fdffd74fbcc9c1abd02e
Lydia Yuzva <lydiaysd@gmail.com>
2019-05-16 11:30:36 +0100

    fix environment js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-run Rails.application
-
-const { environment } = require('@rails/webpacker')
 
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 const webpack = require('webpack')
